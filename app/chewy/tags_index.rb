9453459aae044bc214c3c832fd81ab4406abe920
Barbara <admin@rhabarberbarbara.bar>
2020-12-11 21:52:47 +0100

    elastic search

--- app/chewy/tags_index.rb
+++ app/chewy/tags_index.rb
@@ -2,26 +2,34 @@
 
 class TagsIndex < Chewy::Index
   settings index: { refresh_interval: '15m' }, analysis: {
-    analyzer: {
-      content: {
-        tokenizer: 'keyword',
-        filter: %w(lowercase asciifolding cjk_width),
-      },
+             char_filter: {
+               tsconvert: {
+                 type: 'stconvert',
+                 keep_both: false,
+                 delimiter: '#',
+                 convert_type: 't2s',
+               },
+             },
+             analyzer: {
+               content: {
+                 tokenizer: 'ik_max_word',
+                 filter: %w(lowercase asciifolding cjk_width),
+               },
 
-      edge_ngram: {
-        tokenizer: 'edge_ngram',
-        filter: %w(lowercase asciifolding cjk_width),
-      },
-    },
+               edge_ngram: {
+                 tokenizer: 'edge_ngram',
+                 filter: %w(lowercase asciifolding cjk_width),
+               },
+             },
 
-    tokenizer: {
-      edge_ngram: {
-        type: 'edge_ngram',
-        min_gram: 2,
-        max_gram: 15,
-      },
-    },
-  }
+             tokenizer: {
+               edge_ngram: {
+                 type: 'edge_ngram',
+                 min_gram: 2,
+                 max_gram: 15,
+               },
+             },
+           }
 
   define_type ::Tag.listable, delete_if: ->(tag) { tag.destroyed? || !tag.listable? } do
     root date_detection: false do
